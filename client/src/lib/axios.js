0ad4fac6d0294587252a60986a2cd93ab6bbdb80
jonfry1175 <jonfrymarbun@gmail.com>
2025-05-31 22:07:13 +0700

    feat: Remove .env file and create .env.example with environment variable configurations for development and production

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
+const baseURL = window.location.hostname === 'localhost'
+    ? import.meta.env.VITE_API_URL_DEV
+    : import.meta.env.VITE_API_URL_PROD;
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL,
+    timeout: 5000, // timeout 5 seconds
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
 
 export default axiosInstance
